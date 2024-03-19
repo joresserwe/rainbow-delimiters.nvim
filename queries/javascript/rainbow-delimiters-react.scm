cb8897af631b4dd268975916fe79b1e0a6638b06
Cyan <joresserwe12@gmail.com>
2024-03-20 02:50:26 +0900

    remove tag colour in react

--- queries/javascript/rainbow-delimiters-react.scm
+++ queries/javascript/rainbow-delimiters-react.scm
@@ -55,35 +55,35 @@
 
 
 ;;; React.js support
-(jsx_element
-  open_tag: (jsx_opening_element
-              "<" @delimiter
-              name: (identifier) @delimiter
-              ">" @delimiter)
-  close_tag: (jsx_closing_element
-               "</" @delimiter
-               name: (identifier) @delimiter
-               ">" @delimiter @sentinel)) @container
-
-(jsx_element
-  open_tag: (jsx_opening_element
-              "<" @delimiter
-              name: (member_expression) @delimiter
-              ">" @delimiter)
-  close_tag: (jsx_closing_element
-              "</" @delimiter
-               name: (member_expression) @delimiter
-              ">" @delimiter @sentinel)) @container
-
-(jsx_self_closing_element
-  "<" @delimiter
-  name: (identifier) @delimiter
-  "/>" @delimiter @sentinel) @container
-
-(jsx_self_closing_element
-  "<" @delimiter
-  name: (member_expression) @delimiter
-  "/>" @delimiter @sentinel) @container
+; (jsx_element
+;   open_tag: (jsx_opening_element
+;               "<" @delimiter
+;               name: (identifier) @delimiter
+;               ">" @delimiter)
+;   close_tag: (jsx_closing_element
+;                "</" @delimiter
+;                name: (identifier) @delimiter
+;                ">" @delimiter @sentinel)) @container
+;
+; (jsx_element
+;   open_tag: (jsx_opening_element
+;               "<" @delimiter
+;               name: (member_expression) @delimiter
+;               ">" @delimiter)
+;   close_tag: (jsx_closing_element
+;               "</" @delimiter
+;                name: (member_expression) @delimiter
+;               ">" @delimiter @sentinel)) @container
+;
+; (jsx_self_closing_element
+;   "<" @delimiter
+;   name: (identifier) @delimiter
+;   "/>" @delimiter @sentinel) @container
+;
+; (jsx_self_closing_element
+;   "<" @delimiter
+;   name: (member_expression) @delimiter
+;   "/>" @delimiter @sentinel) @container
 
 (jsx_expression
   "{" @delimiter
